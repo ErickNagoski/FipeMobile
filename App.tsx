ea64bb70b50f5c6228585a61230d55326221c8dc
Erick Nagoski <ericknagoski@gmail.com>
2021-08-09 17:05:09 -0300

    Fist commit, request brands

--- App.tsx
+++ App.tsx
@@ -1,21 +1,162 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Platform, Text, View, SafeAreaView, Button } from 'react-native';
+import { Picker } from "@react-native-picker/picker";
+
+import api from "./api";
+
+
 
 export default function App() {
+  const [vehicles, setVehicles] = useState(["Carros", "Motos", "Caminhões"]);
+  const [vehicleSelected, setVehicleSelected] = useState<string>();
+
+  const [brands, setBrands] = useState(["mercedes", "fiat", "jeep", "chevrolet", "honda"]);
+  const [brandSelected, setBrandSelected] = useState<string>();
+
+  const [models, setModels] = useState(["gol", "uno", "palio", "corsa", "saveiro", "fox"]);
+  const [modelSelected, setModelSelected] = useState<string>();
+
+  const [years, setYears] = useState(["2002", "2003", "2005", "2006", "2009", "20010", "2020"]);
+  const [yearSelected, setYearSelected] = useState<string>();
+
+  interface BrandsProps {
+    nome: string;
+    codigo: string;
+  }
+
+
+  async function loadBrands() {
+    await api.get(`${vehicleSelected}/marcas`).then(function (response) {
+      console.log(response)
+    })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>FipeMobile</Text>
+      </View>
+
+
+
+      <View style={styles.content}>
+        <Text style={styles.title}>Selecione o tipo de veículo para a consulta</Text>
+        <Picker
+          selectedValue={vehicleSelected}
+          style={styles.vehiclePicker}
+          onValueChange={(itemValue) =>
+            setVehicleSelected(itemValue)
+          }>
+          {
+            vehicles.map(vh => {
+              return <Picker.Item label={vh} value={vh} />
+            })
+          }
+        </Picker>
+
+
+      </View>
+
+      <View style={styles.selectContainer}>
+        <Picker
+          selectedValue={vehicleSelected}
+          style={styles.picker}
+          onValueChange={(itemValue) =>
+            setBrandSelected(itemValue)
+          }>
+          {
+            brands.map(vh => {
+              return <Picker.Item label={vh} value={vh} />
+            })
+          }
+        </Picker>
+        <Picker
+          selectedValue={vehicleSelected}
+          style={styles.picker}
+          onValueChange={(itemValue) =>
+            setModelSelected(itemValue)
+          }>
+          {
+            models.map(vh => {
+              return <Picker.Item label={vh} value={vh} />
+            })
+          }
+        </Picker>
+        <Picker
+          selectedValue={vehicleSelected}
+          style={styles.picker}
+          onValueChange={(itemValue) =>
+            setYearSelected(itemValue)
+          }>
+          {
+            years.map(vh => {
+              return <Picker.Item label={vh} value={vh} />
+            })
+          }
+        </Picker>
+      </View>
+      <Button
+        title={"Cadastrar planta"}
+        onPress={loadBrands}
+      />
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#FFFF",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+
   },
+  header: {
+    height: 50,
+    backgroundColor: "#02b7f2",
+    display: "flex",
+    alignItems: "center"
+  },
+  headerText: {
+    fontSize: 32,
+    color: "white",
+    fontWeight: "bold",
+  },
+
+  content: {
+    padding: 15,
+
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+
+  },
+  vehiclePicker: {
+    height: 50,
+    width: 200,
+    borderWidth: 2,
+    borderColor: "#02b7f2",
+  },
+  picker: {
+    height: 50,
+    width: 130,
+    borderWidth: 2,
+    borderColor: "#02b7f2",
+  },
+
+  selectContainer: {
+    backgroundColor: "red",
+    display: "flex",
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+  }
 });
